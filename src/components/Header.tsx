fc2d968e17811ac0a2219c7c8ca045b923e176ad
jonathanprozzi <jonathanprozzi@gmail.com>
2020-06-02 17:53:03 -0400

    added Header component, modified import for Footer until i change component

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Flex, Heading, Link as ChakraLink, PseudoBox } from "@chakra-ui/core";
+
+const Header: React.FC = () => {
+  return (
+    <Flex
+      direction="column"
+      justify="center"
+      align="center"
+      height="8vh"
+      w="100vw"
+      bg="black"
+      boxShadow="md"
+    >
+      <Flex
+        direction="row"
+        justify="space-around"
+        w={["100vw", "100vw", "80vw", "80vw"]}
+      >
+        <Link href="/" passHref>
+          <ChakraLink color="cyan.100">
+            <Heading as="h1" size="lg" color="white">
+              Learning Library v1
+            </Heading>
+          </ChakraLink>
+        </Link>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Header;
--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -1,5 +1,5 @@
 import Header from "./Header";
-import Footer from "./Footer";
+import { Footer } from "./Footer";
 import { Box, Flex } from "@chakra-ui/core";
 
 const SiteLayout: React.FC = ({ children }) => {
