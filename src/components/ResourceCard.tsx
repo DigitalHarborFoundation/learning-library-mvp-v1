1d6d4ed5aa7a2c755c95261f570ef7220456ff6b
jonathanprozzi <jonathanprozzi@gmail.com>
2020-06-10 10:28:58 -0400

    working on the card grid UI

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -8,6 +8,7 @@ import {
   ButtonGroup,
   Button,
   Flex,
+  Tag,
 } from "@chakra-ui/core";
 
 const ResourceCard: React.FC = ({ data }) => {
@@ -17,36 +18,44 @@ const ResourceCard: React.FC = ({ data }) => {
       h="auto"
       maxW="md"
       overflow="hidden"
-      rounded="md"
-      boxShadow="0px 4px 10px rgba(0, 0, 0, 0.2);"
-      _hover={{
-        cursor: "pointer",
-        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.5)",
-      }}
+      rounded="lg"
+      borderWidth="1px"
+      // boxShadow="0px 4px 10px rgba(0, 0, 0, 0.2);"
+      // _hover={{
+      //   cursor: "pointer",
+      //   boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.5)",
+      // }}
     >
       <AspectRatioBox height="400px" ratio={16 / 9}>
         <Image
           src={data.image[0].url}
           alt="Cup of coffee with 'Begin' on it sitting on a wooden table"
-          rounded="md"
           objectFit="cover"
         />
       </AspectRatioBox>
       <Box p="4" alignItems="center" justifyContent="center">
         <Box d="flex" alignItems="baseline">
-          <Box
-            as="h3"
-            color="gray.500"
-            fontWeight="semibold"
-            letterSpacing="wide"
-            fontSize="xs"
-            textTransform="uppercase"
-            ml={0}
-          >
-            {data.title}
-          </Box>
+          <Flex direction="row">
+            <Badge rounded="md" marginRight="1" variantColor="purple">
+              {data.os}
+            </Badge>
+            <Badge rounded="md" marginLeft="1" variantColor="teal">
+              {data.pathway}
+            </Badge>
+          </Flex>
         </Box>
-
+        <Box
+          as="h3"
+          color="gray.500"
+          fontWeight="semibold"
+          letterSpacing="wide"
+          fontSize="xs"
+          textTransform="uppercase"
+          ml={0}
+          paddingY={2}
+        >
+          {data.title}
+        </Box>
         <Button
           rightIcon="view"
           variantColor="gray"
