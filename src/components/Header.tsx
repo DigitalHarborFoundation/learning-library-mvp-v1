a59d2a436620f627d05100a6e51d08211e4738ae
jonathanprozzi <jonathanprozzi@gmail.com>
2020-06-18 09:20:45 -0400

    added icon to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,13 @@
 import Link from "next/link";
-import { Flex, Heading, Link as ChakraLink, PseudoBox } from "@chakra-ui/core";
+import {
+  Box,
+  Flex,
+  Heading,
+  Link as ChakraLink,
+  PseudoBox,
+  Stack,
+} from "@chakra-ui/core";
+import { GiBookshelf } from "react-icons/gi";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 
 const Header: React.FC = () => {
@@ -19,11 +27,16 @@ const Header: React.FC = () => {
         w={["100vw", "100vw", "80vw", "80vw"]}
       >
         <Link href="/" passHref>
-          <ChakraLink color="cyan.100">
-            <Heading as="h1" size="lg" color="white">
-              Learning Library
-            </Heading>
-          </ChakraLink>
+          {/* <ChakraLink color="cyan.100"> */}
+          <a>
+            <Flex direction="row" align="center">
+              <Box as={GiBookshelf} size="32px" color="white" marginRight={4} />
+              <Heading as="h1" size="lg" color="white">
+                Learning Library
+              </Heading>
+            </Flex>
+            {/* </ChakraLink> */}
+          </a>
         </Link>
         {/* <DarkModeSwitch /> */}
       </Flex>
