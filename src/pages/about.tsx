1eb6d97a21a01a90802894ab1555d92867fed294
jonathanprozzi <jonathanprozzi@gmail.com>
2020-06-24 13:47:27 -0400

    added /help and /about and added boilerplate content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,27 @@
-import useSWR from "swr";
-import { Flex, Heading } from "@chakra-ui/core";
 import { NextPage } from "next";
+import Link from "next/link";
+import { Flex, Box, Heading, Link as ChakraLink } from "@chakra-ui/core";
 
 const AboutPage: NextPage = () => {
   return (
-    <Flex
-      direction="column"
-      align="center"
-      justify="center"
-      maxWidth="960"
-      margin="0 auto"
-    >
-      <Heading as="h2">About!</Heading>
+    <Flex direction="column" align="center" justify="center">
+      <Box
+        maxWidth="960px"
+        minWidth="960px"
+        minHeight="80vh"
+        marginY={8}
+        paddingX={8}
+        paddingY={4}
+        bg="white"
+        rounded="md"
+      >
+        <Heading as="h2" textAlign="center">
+          Learning Library
+        </Heading>
+        <Link href="/resources">
+          <ChakraLink>Link to something?</ChakraLink>
+        </Link>
+      </Box>
     </Flex>
   );
 };
