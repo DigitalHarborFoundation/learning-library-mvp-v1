e6fcbde8b0b4d8dab7298921ce5c43ce795de1c0
jonathanprozzi <jonathanprozzi@gmail.com>
2020-06-25 13:26:06 -0400

    added copy to /help

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,6 +1,14 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import { Flex, Box, Heading, Link as ChakraLink } from "@chakra-ui/core";
+import {
+  Flex,
+  Box,
+  Heading,
+  Link as ChakraLink,
+  Stack,
+  Text,
+  Icon,
+} from "@chakra-ui/core";
 
 const HelpPage: NextPage = () => {
   return (
@@ -11,16 +19,46 @@ const HelpPage: NextPage = () => {
       <Box
         maxWidth="960px"
         minWidth="960px"
-        minHeight="80vh"
         marginY={8}
         paddingX={8}
         paddingY={4}
         bg="white"
         rounded="md"
       >
-        <Link href="/help">
-          <ChakraLink>Go to Help!</ChakraLink>
-        </Link>
+        <Stack spacing={4}>
+          <Text>
+            Asking for help is a crucial skill in learning. DHF wants to help!
+          </Text>
+          <Text>
+            Please let us know what questions you have by submitting a help
+            ticket. Follow{" "}
+            <ChakraLink
+              href="https://digitalharborhelp.freshdesk.com/support/tickets/new"
+              isExternal
+              color="blue.500"
+            >
+              this link to the DHF help desk
+            </ChakraLink>{" "}
+            and include your name, an email address, a title that describes your
+            problem, and then a more detailed description of your problem or
+            question.
+          </Text>
+          <Text>
+            DHF staff will do their best to get back to you promptly. If you
+            have additional questions or concerns, please email{" "}
+            <ChakraLink
+              href="mailto: contact@digitalharbor.org"
+              isExternal
+              color="blue.500"
+            >
+              contact@digitalharbor.org
+            </ChakraLink>
+          </Text>
+          <Text as="em">
+            We hope to increase support throughout July. Please check your
+            emails and refer to this page for updates.
+          </Text>
+        </Stack>
       </Box>
     </Flex>
   );
