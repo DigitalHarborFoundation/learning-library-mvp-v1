d1f54fbd43116479e1e6172353a862e758474c8c
jonathanprozzi <jonathanprozzi@gmail.com>
2020-06-26 22:04:37 -0400

    adjusted header layout sizing for mobile - working on some responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,17 +22,19 @@ const Header: React.FC = () => {
       w="100vw"
       bg="black"
       boxShadow="md"
+      paddingY={8}
     >
       <Flex
         direction="row"
         justify="space-around"
-        w={["100vw", "100vw", "80vw", "80vw"]}
+        width={["100vw", "100vw", "80vw", "80vw"]}
+        paddingX={[4, 4, 6, 8]}
       >
         <Link href="/" passHref>
           <a>
             <Flex direction="row" align="center">
               <Box as={GiBookshelf} size="32px" color="white" marginRight={4} />
-              <Heading as="h1" size="lg" color="white">
+              <Heading as="h1" size="md" color="white">
                 Learning Library
               </Heading>
             </Flex>
