7fb3c654d6134d9b724ef1ccd47f1e36b424232f
jonathanprozzi <jonathanprozzi@gmail.com>
2020-09-24 13:20:38 -0400

    added next-seo for improved seo/meta/og

---
+++ src/pages/playlists/project-waves.tsx
@@ -0,0 +1,47 @@
+import { Heading } from "@chakra-ui/core";
+import PlaylistsData from "../../components/PlaylistData";
+
+const ProjectWavesPage = ({
+  title,
+  landingPageCopy,
+  image,
+  resourceLinks,
+  linkTitles,
+}) => {
+  return (
+    <PlaylistsData
+      title={title}
+      landingPageCopy={landingPageCopy}
+      image={image}
+      resourceLinks={resourceLinks}
+      linkTitles={linkTitles}
+    ></PlaylistsData>
+  );
+};
+
+export default ProjectWavesPage;
+
+export async function getServerSideProps({ params, query }) {
+  const apiKey = process.env.API_KEY;
+  const baseId = process.env.BASE_ID;
+
+  const res = await fetch(
+    `https://api.airtable.com/v0/${baseId}/Playlists/rec6p6w4hz9zzeOQ3?api_key=${apiKey}`,
+    { method: "GET", mode: "no-cors", credentials: "same-origin" }
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      id: data.id || null,
+      title: data.fields["Playlist Title"] || null,
+      resourceLinks: data.fields["Resource Links"] || null,
+      linkTitles: data.fields["Link Titles"] || null,
+      image: data.fields["Featured Image"][0].url || null,
+      description: data.fields["Description"] || null,
+      level: data.fields["Skill Level"] || null,
+      tags: data.fields["Tags"] || null,
+      landingPageCopy: data.fields["Landing Page Copy"] || null,
+    },
+  };
+}
