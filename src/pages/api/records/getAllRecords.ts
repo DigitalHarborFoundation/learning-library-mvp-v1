38b1f9a7e905e11824c46a88c3dded7b139993d2
jonathanprozzi <jonathanprozzi@gmail.com>
2020-10-06 15:25:04 -0400

    split off helper functions into a new /api/utils/airtable.ts file

--- src/pages/api/records/getAllRecords.ts
+++ src/pages/api/records/getAllRecords.ts
@@ -1,22 +1,4 @@
-import { __recordsPerPageForIteration } from "airtable/lib/table";
-
-const Airtable = require("airtable");
-const base = new Airtable({ apiKey: process.env.API_KEY }).base(
-  process.env.BASE_ID
-);
-const table = base(process.env.TABLE_NAME);
-
-// nice tricks from James Q Quick!
-const getMinifiedRecord = (record) => {
-  return {
-    id: record.id,
-    fields: record.fields,
-  };
-};
-
-const minifyRecords = (records) => {
-  return records.map((record) => getMinifiedRecord(record));
-};
+import { table, minifyRecords } from "./utils/airtable";
 
 const getAllRecords = async (req, res) => {
   const records = await table
