e725b64fa5f0514bc69361b9e2139ae7300e810c
jonathanprozzi <jonathanprozzi@gmail.com>
2020-10-30 13:03:52 -0400

    updated _app: changed ThemeProvider to ChakraProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import {
-  ThemeProvider,
+  ChakraProvider,
   theme,
   CSSReset,
   Flex,
@@ -12,10 +12,9 @@ import SiteLayout from "../components/SiteLayout";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider>
+    <ChakraProvider resetCSS={true}>
       {/* <ColorModeProvider> */}
       <Flex direction="column" align="center" justify="center">
-        <CSSReset />
         <SiteLayout>
           <DefaultSeo
             title="Learning Library"
@@ -43,7 +42,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         </SiteLayout>
       </Flex>
       {/* </ColorModeProvider> */}
-    </ThemeProvider>
+    </ChakraProvider>
   );
 };
 
